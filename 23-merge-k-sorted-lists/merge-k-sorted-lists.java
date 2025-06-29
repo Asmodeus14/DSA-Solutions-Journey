68fef49f297aaa4d02047e01a9ce6d19703920d1
Abhay Singh <154667932+Asmodeus14@users.noreply.github.com>
2025-06-29 08:25:09 +0530

    Time: 4 ms (77.10%) | Memory: 44.8 MB (20.39%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -1,5 +1,43 @@
+import com.eclipsesource.json.JsonArray;
+import com.eclipsesource.json.JsonValue;
 import java.util.PriorityQueue;
 import java.util.Comparator;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    // Converts a JSON array to a linked list
+    public static ListNode arrayToListNode(JsonArray jsonArray) {
+        if (jsonArray == null || jsonArray.size() == 0) {
+            return null;
+        }
+        ListNode dummy = new ListNode(-1);
+        ListNode current = dummy;
+        for (JsonValue value : jsonArray) {
+            current.next = new ListNode(value.asInt());
+            current = current.next;
+        }
+        return dummy.next;
+    }
+
+    // Converts a JSON array of arrays to an array of linked lists
+    public static ListNode[] arrayToListNodeArray(JsonArray jsonArrayOfArrays) {
+        if (jsonArrayOfArrays == null || jsonArrayOfArrays.size() == 0) {
+            return new ListNode[0];
+        }
+        ListNode[] lists = new ListNode[jsonArrayOfArrays.size()];
+        int index = 0;
+        for (JsonValue arrayValue : jsonArrayOfArrays) {
+            JsonArray innerArray = arrayValue.asArray();
+            lists[index++] = arrayToListNode(innerArray);
+        }
+        return lists;
+    }
+}
 
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
@@ -8,6 +46,7 @@ class Solution {
         }
         
         PriorityQueue<ListNode> minHeap = new PriorityQueue<>(new Comparator<ListNode>() {
+            @Override
             public int compare(ListNode a, ListNode b) {
                 return a.val - b.val;
             }
@@ -19,7 +58,7 @@ class Solution {
             }
         }
         
-        ListNode dummy = new ListNode(0);
+        ListNode dummy = new ListNode(-1);
         ListNode current = dummy;
         
         while (!minHeap.isEmpty()) {
