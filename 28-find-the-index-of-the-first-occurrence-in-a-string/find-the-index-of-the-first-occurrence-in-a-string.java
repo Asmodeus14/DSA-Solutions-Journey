450a2d89418bba8d7609ffc729dae702ff22e905
Abhay Singh <154667932+Asmodeus14@users.noreply.github.com>
2025-07-06 14:28:44 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (89.36%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,7 +1,6 @@
-import java.io.*;
+
 class Solution {
     public int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);
-        
+        return haystack.indexOf(needle); 
     }
 }
