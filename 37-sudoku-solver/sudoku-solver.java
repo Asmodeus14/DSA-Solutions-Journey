77eb531571ed0031fd1fdfb69b48dc8f75ee15af
Abhay Singh <154667932+Asmodeus14@users.noreply.github.com>
2025-07-09 10:55:09 +0530

    Time: 157 ms (87.89%) | Memory: 41.5 MB (17.20%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.java
@@ -0,0 +1,45 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board, 0, 0);
+    }
+    
+    private boolean solve(char[][] board, int row, int col) {
+        if (row == 9) {
+            return true;
+        }
+        if (col == 9) {
+            return solve(board, row + 1, 0);
+        }
+        if (board[row][col] != '.') {
+            return solve(board, row, col + 1);
+        }
+        
+        for (char c = '1'; c <= '9'; c++) {
+            if (isValid(board, row, col, c)) {
+                board[row][col] = c;
+                if (solve(board, row, col + 1)) {
+                    return true;
+                }
+                board[row][col] = '.';
+            }
+        }
+        return false;
+    }
+    
+    private boolean isValid(char[][] board, int row, int col, char c) {
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == c) return false;
+            if (board[row][i] == c) return false;
+        }
+        int boxRowStart = 3 * (row / 3);
+        int boxColStart = 3 * (col / 3);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[boxRowStart + i][boxColStart + j] == c) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
